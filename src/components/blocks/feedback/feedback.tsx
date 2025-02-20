a6078dcc9784a361056ff0b3a0d048c2ee534300
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2025-02-20 20:16:08 +0300

    feedback photos

--- src/components/blocks/feedback/feedback.tsx
+++ src/components/blocks/feedback/feedback.tsx
@@ -1,12 +1,24 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import feedback1 from '../../../assets/feedback/feedback-1.webp';
-import feedback2 from '../../../assets/feedback/feedback-2.webp';
-import feedback3 from '../../../assets/feedback/feedback-3.webp';
-import feedback4 from '../../../assets/feedback/feedback-4.webp';
-import feedback5 from '../../../assets/feedback/feedback-5.webp';
-import feedback6 from '../../../assets/feedback/feedback-6.webp';
-import feedback7 from '../../../assets/feedback/feedback-7.webp';
+import feedback1 from '../../../assets/feedback/feedback-1.png';
+import feedback2 from '../../../assets/feedback/feedback-2.png';
+import feedback3 from '../../../assets/feedback/feedback-3.png';
+import feedback4 from '../../../assets/feedback/feedback-4.png';
+import feedback5 from '../../../assets/feedback/feedback-5.png';
+import feedback6 from '../../../assets/feedback/feedback-6.png';
+import feedback7 from '../../../assets/feedback/feedback-7.png';
+import feedback8 from '../../../assets/feedback/feedback-8.png';
+import feedback9 from '../../../assets/feedback/feedback-9.png';
+import feedback10 from '../../../assets/feedback/feedback-10.png';
+import feedback11 from '../../../assets/feedback/feedback-11.png';
+import feedback12 from '../../../assets/feedback/feedback-12.png';
+import feedback13 from '../../../assets/feedback/feedback-13.png';
+import feedback14 from '../../../assets/feedback/feedback-14.png';
+import feedback15 from '../../../assets/feedback/feedback-15.png';
+import feedback16 from '../../../assets/feedback/feedback-16.png';
+import feedback17 from '../../../assets/feedback/feedback-17.png';
+import feedback18 from '../../../assets/feedback/feedback-18.png';
+
 import H2 from '../../ui/h2/h2';
 //
 import style from './feedback.module.scss';
@@ -47,7 +59,7 @@ function Feedback () {
                 }}
             pagination={true}
             className={style.swiper}
-        >
+            >
             <SwiperSlide className={style.slide}>
                 <div className={style.slideWrap}>
                     <img className={style.img} src={feedback1} alt="" />
@@ -81,6 +93,61 @@ function Feedback () {
             <SwiperSlide className={style.slide}>
                 <div className={style.slideWrap}>
                     <img className={style.img} src={feedback7} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback8} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback9} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback10} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback11} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback12} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback13} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback14} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback15} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback16} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback17} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback18} alt="" />
                 </div>
             </SwiperSlide>
         </Swiper>
@@ -117,7 +184,7 @@ function Feedback () {
             pagination={true}
             className={style.swiper}
         >
-            <SwiperSlide className={style.slide}>
+                        <SwiperSlide className={style.slide}>
                 <div className={style.slideWrap}>
                     <img className={style.img} src={feedback1} alt="" />
                 </div>
@@ -150,6 +217,61 @@ function Feedback () {
             <SwiperSlide className={style.slide}>
                 <div className={style.slideWrap}>
                     <img className={style.img} src={feedback7} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback8} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback9} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback10} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback11} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback12} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback13} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback14} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback15} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback16} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback17} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className={style.slide}>
+                <div className={style.slideWrap}>
+                    <img className={style.img} src={feedback18} alt="" />
                 </div>
             </SwiperSlide>
         </Swiper>
