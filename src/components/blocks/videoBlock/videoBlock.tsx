d1eae92012790dd821cc46d8d00a207eaf793c2e
Verona4321 <sverona432@yandex.ru>
2025-02-21 23:14:57 +0400

    preview

--- src/components/blocks/videoBlock/videoBlock.tsx
+++ src/components/blocks/videoBlock/videoBlock.tsx
@@ -4,7 +4,7 @@ import Button from '../../ui/button/button';
 import training from '../../../assets/videos/training.mp4'; 
 import trainingMobile from '../../../assets/videos/video.mp4'; 
 import previewImage from '../../../../src/assets/photos/video.png'; 
-import previewImageMobile from '../../../../src/assets/photos/videoMobile2.png'; 
+import previewImageMobile from '../../../../src/assets/photos/videoMobile.png'; 
 
 import style from './videoBlock.module.scss'; 
 
